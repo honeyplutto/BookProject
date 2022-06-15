2b775ffb79c01f1a8561a6298ade1c06320dc7a8
Albert <khachikyanalber2019@gmail.com>
2022-06-15 15:18:44 +0400

    GET, POST, PUT, DELETE with MongoDB

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,9 +1,10 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+const Goal = require('../model/goalModel');
 
 const getGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'get'
-    })
+    const goals = await Goal.find();
+
+    res.status(200).json(goals);
 });
 
 const setGoal = asyncHandler(async (req,res) => {
@@ -11,21 +12,45 @@ const setGoal = asyncHandler(async (req,res) => {
         res.status(400)
         throw new Error('Please add a text field');
     }
-    res.status(200).json({
-        message: 'post'
+
+    const goal = await Goal.create({
+        text: req.body.text
     })
+
+    res.status(200).json(goal)
 });
 
 const updateGoal = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message: `put ${req.params.id}`
-    })
+
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    const updateGoal = await Goal.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        {
+            new: true,
+        });
+
+    res.status(200).json(updateGoal)
 });
 
 const deleteGoal = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message: `delete ${req.params.id}`
-    })
+
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    await goal.remove();
+
+    res.status(200).json({id: req.params.id})
 });
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
